940208292bf3c4288d6b8a97737019e859435a42
Elin Asplund <elin.asplund91@gmail.com>
2023-04-06 14:33:12 +0200

    made better products routing

--- Bmerketo-WebApp/Controllers/ProductsController.cs
+++ Bmerketo-WebApp/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bmerketo_WebApp.Models;
+using Bmerketo_WebApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bmerketo_WebApp.Controllers
 {
@@ -8,6 +10,32 @@ namespace Bmerketo_WebApp.Controllers
         {
             ViewData["Title"] = "Products";
             return View();
+        }
+        
+        public IActionResult Details()
+        {
+            ViewData["Title"] = "Product Details";
+
+			var viewModel = new ProductDetailsViewModel
+			{
+				ShopHero = new HeroModel
+				{
+					Heading = "SHOP",
+					Subheading = "HOME. PRODUCT DETAILS"
+				},
+				RelatedShoes = new RelatedProductsViewModel
+				{
+					GridCards = new List<GridCollectionCardViewModel>
+					{
+						new GridCollectionCardViewModel{ Id ="1", Title = "Apple watch series", Price = 10, ImageUrl = "/images/product.jpg" },
+						new GridCollectionCardViewModel{ Id ="2", Title = "Apple watch series", Price = 20, ImageUrl = "/images/product.jpg" },
+						new GridCollectionCardViewModel{ Id ="3", Title = "Apple watch series", Price = 30, ImageUrl = "/images/product.jpg" },
+						new GridCollectionCardViewModel{ Id ="4", Title = "Apple watch series", Price = 40, ImageUrl = "/images/product.jpg" }
+					}
+				}
+			};
+
+			return View(viewModel);
         }
     }
 }
