c1b8b69effc687213b67bc73d45c0174681110ae
Elin Asplund <elin.asplund91@gmail.com>
2023-04-06 14:13:46 +0200

    made a better account routing

---
+++ Bmerketo-WebApp/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bmerketo_WebApp.Controllers
+{
+	public class AccountController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		
+		public IActionResult Register()
+		{
+			ViewData["Title"] = "Register";
+			return View();
+		}
+		
+		public IActionResult Login()
+		{
+			ViewData["Title"] = "Login";
+			return View();
+		}
+	}
+}
