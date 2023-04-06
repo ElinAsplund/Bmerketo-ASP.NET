47060282574d9ecd8c774f1aed291c6e7acf0d10
Elin Asplund <elin.asplund91@gmail.com>
2023-04-07 00:42:59 +0200

    structuring of the register form

--- Bmerketo-WebApp/Controllers/AccountController.cs
+++ Bmerketo-WebApp/Controllers/AccountController.cs
@@ -1,24 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bmerketo_WebApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Bmerketo_WebApp.Controllers
+namespace Bmerketo_WebApp.Controllers;
+
+public class AccountController : Controller
 {
-	public class AccountController : Controller
+	public IActionResult Index()
 	{
-		public IActionResult Index()
+		ViewData["Title"] = "Account";
+
+		return View();
+	}
+	
+	public IActionResult Register()
+	{
+		ViewData["Title"] = "Register";
+		return View();
+	}
+
+	[HttpPost]
+	public IActionResult Register(AccountRegisterViewModel accountRegisterViewModel)
+	{
+		ViewData["Title"] = "Register";
+
+		if(ModelState.IsValid)
 		{
-			return View();
+
 		}
-		
-		public IActionResult Register()
-		{
-			ViewData["Title"] = "Register";
-			return View();
-		}
-		
-		public IActionResult Login()
-		{
-			ViewData["Title"] = "Login";
-			return View();
-		}
+
+		return View(accountRegisterViewModel);
+	}
+	
+	public IActionResult Login()
+	{
+		ViewData["Title"] = "Login";
+		return View();
 	}
 }
---
+++ Bmerketo-WebApp/ViewModels/AccountRegisterViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bmerketo_WebApp.ViewModels;
+
+public class AccountRegisterViewModel
+{
+	[Display(Name = "First Name*")]
+	[Required(ErrorMessage = "Please enter your First Name.")]
+	[RegularExpression(@"^[a-öA-Ö]+(?:[ é'-][a-öA-Ö]+)*$", ErrorMessage = "You need to enter a valid First Name.")]
+	public string FirstName { get; set; } = null!;
+	
+
+	[Display(Name = "Last Name*")]
+	[Required(ErrorMessage = "Please enter your Last Name.")]
+	[RegularExpression(@"^[a-öA-Ö]+(?:[ é'-][a-öA-Ö]+)*$", ErrorMessage = "You need to enter a valid Last Name.")]
+	public string LastName { get; set; } = null!;
+	
+
+	[Display(Name = "Street Name*")]
+	[Required(ErrorMessage = "Please enter your Street Name.")]
+	public string StreetName { get; set; } = null!;
+
+
+	[Display(Name = "Postal Code*")]
+	[Required(ErrorMessage = "Please enter your Postal Code.")]
+	public string PostalCode { get; set; } = null!;
+
+
+	[Display(Name = "City*")]
+	[Required(ErrorMessage = "Please enter your City.")]
+	public string City { get; set; } = null!;
+
+
+	[Display(Name = "Mobile (optional)")]
+	public string? PhoneNumber { get; set; }
+
+
+	[Display(Name = "Company (optional)")]
+	public string? Company { get; set; }
+
+
+	[Display(Name = "E-mail*")]
+	[DataType(DataType.EmailAddress)]
+	[Required(ErrorMessage = "Please enter your E-mail.")]
+	[RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "You need to enter a valid E-mail.")]
+	public string Email { get; set; } = null!;
+
+
+	[Display(Name = "Password*")]
+	[DataType(DataType.Password)]
+	[Required(ErrorMessage = "Please enter a Password.")]
+	[RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$", ErrorMessage = "You need to enter a valid Password.")]
+	public string Password { get; set; } = null!;
+
+
+	[Display(Name = "Confirm Password*")]
+	[DataType(DataType.Password)]
+	[Required(ErrorMessage = "Please Confirm the Password.")]
+	[Compare(nameof(Password), ErrorMessage = "The Passwords don't match.")]
+	public string ConfirmPassword { get; set; } = null!;
+
+
+	[Display(Name = "Upload Profile Image (optional)")]
+	public string? ProfileImage { get; set; }
+
+	[Required(ErrorMessage = "Please Accept the Terms.")]
+	public bool AcceptsTerms { get; set; } = false;
+}
