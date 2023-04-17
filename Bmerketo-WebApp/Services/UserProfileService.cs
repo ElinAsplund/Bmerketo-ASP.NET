759cd60895c7f437b87999cd83dee71c262810c7
Elin Asplund <elin.asplund91@gmail.com>
2023-04-18 00:20:02 +0200

    simple information rendering of the logged in user

--- Bmerketo-WebApp/Services/UserProfileService.cs
+++ Bmerketo-WebApp/Services/UserProfileService.cs
@@ -1,5 +1,6 @@
 ﻿using Bmerketo_WebApp.Contexts;
 using Bmerketo_WebApp.Models.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bmerketo_WebApp.Services;
@@ -17,5 +18,11 @@ public class UserProfileService
 	{
 		var userProfileEntity = await _identityContext.UserProfiles.Include(x => x.User).FirstOrDefaultAsync(x => x.UserId == userId);
 		return userProfileEntity!;
+	}
+	public async Task<IdentityUser> GetIdentityUserAsync(string email)
+	{
+		var identityUser = await _identityContext.Users.FirstOrDefaultAsync(x => x.Email == email);
+
+		return identityUser!;
 	}
 }
