0749494509be947ea34e12a6f50fde617936b2d2
Elin Asplund <elin.asplund91@gmail.com>
2023-04-19 15:01:40 +0200

    more simplification of the JS-code

--- Bmerketo-WebApp/wwwroot/js/site.js
+++ Bmerketo-WebApp/wwwroot/js/site.js
@@ -35,7 +35,7 @@ function footerPosition(element, scrollHeight, innerHeight) {
 footerPosition('footer', document.body.scrollHeight, window.innerHeight)
 
 
-//Validations!
+//Validations
 function removeAspValidation(errorSpan) {
     if (errorSpan.innerText !== null)
         errorSpan.classList.add("d-none")
@@ -43,16 +43,6 @@ function removeAspValidation(errorSpan) {
 
 function validateInputSwitch(element, errorMsg, label) {
     errorMsg.innerText = "";
-
-    //--THIS PART, NOT NEEDED?--
-    firstNameOK = false;
-    lastNameOK = false;
-    streetNameOK = false;
-    postalCodeOK = false;
-    cityOK = false;
-    emailOK = false;
-    passwordOK = false;
-    //------------------------
 
     const regExName = /^[a-öA-Ö]+(?:[ é'-][a-öA-Ö]+)*$/;
     const regExEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -68,7 +58,7 @@ function validateInputSwitch(element, errorMsg, label) {
                 errorMsg.innerText = `Your ${label} must contain at least 2 characters!`;
 
             if (!regExName.test(element.value))
-                errorMsg.innerText = `Please enter a valid ${label}.`;
+                errorMsg.innerText = `Please enter a ${label}.`;
 
             if (errorMsg.innerText === "")
                 firstNameOK = true;
@@ -80,7 +70,7 @@ function validateInputSwitch(element, errorMsg, label) {
                 errorMsg.innerText = `Your ${label} must contain at least 2 characters!`;
 
             if (!regExName.test(element.value))
-                errorMsg.innerText = `Please enter a valid ${label}.`;
+                errorMsg.innerText = `Please enter a ${label}.`;
 
             if (errorMsg.innerText === "")
                 lastNameOK = true;
@@ -88,29 +78,20 @@ function validateInputSwitch(element, errorMsg, label) {
             break;
 
         case "street name":
-            if (errorMsg.innerText === "")
-                streetNameOK = true;
-
             break;
 
         case "postal code":
-            if (errorMsg.innerText === "")
-                postalCodeOK = true;
-
             break;
 
         case "city":
-            if (errorMsg.innerText === "")
-                cityOK = true;
-
             break;
 
         case "email":
-            if (element.value === "")
-                errorMsg.innerText = `Please enter an ${label}.`;
-
             if (!regExEmail.test(element.value))
                 errorMsg.innerText = `Please enter a valid ${label}.`;
+
+            if (element.value === "")
+                errorMsg.innerText = `Please enter an ${label}.`;
 
             if (errorMsg.innerText === "")
                 emailOK = true;
@@ -142,276 +123,88 @@ function validateInputSwitch(element, errorMsg, label) {
             break;
 
     }
-
-    //if (firstNameOK && lastNameOK && streetNameOK && postalCodeOK && cityOK && emailOK && passwordOK && confirmPasswordOK) {
-    //    console.log("true")
-    //    return true;
-    //}
 }
 
-function validateFirstNameSwitch(event) {
+function validateInput(event) {
     try {
-        const errorMsg = document.querySelector('#first-name-error');
-        const errorSpan = document.querySelector('#first-name-span');
         let element = event.target;
+        const errorMsg = document.querySelector(`#${element.id}-error`);
+        const errorSpan = document.querySelector(`#${element.id}-span`);
+
+        let inputName = (element.id).replace("-", " ");
 
         removeAspValidation(errorSpan)
 
-        if (validateInputSwitch(element, errorMsg, "first name"))
+        if (validateInputSwitch(element, errorMsg, inputName))
             return true;
 
     } catch { }
 }
-
-function validateLastNameSwitch(event) {
-    try {
-        const errorMsg = document.querySelector('#last-name-error');
-        const errorSpan = document.querySelector('#last-name-span');
-        let element = event.target;
-
-        removeAspValidation(errorSpan)
-
-        if (validateInputSwitch(element, errorMsg, "last name"))
-            return true;
-
-    } catch { }
-}
-
-function validateStreetNameSwitch(event) {
-    try {
-        const errorMsg = document.querySelector('#street-name-error');
-        const errorSpan = document.querySelector('#street-name-span');
-        let element = event.target;
-
-        removeAspValidation(errorSpan)
-
-        if (validateInputSwitch(element, errorMsg, "street name"))
-            return true;
-
-    } catch { }
-}
-
-function validatePostalCodeSwitch(event) {
-    try {
-        const errorMsg = document.querySelector('#postal-code-error');
-        const errorSpan = document.querySelector('#postal-code-span');
-        let element = event.target;
-
-        removeAspValidation(errorSpan)
-
-        if (validateInputSwitch(element, errorMsg, "postal code"))
-            return true;
-
-    } catch { }
-}
-
-function validateCitySwitch(event) {
-    try {
-        const errorMsg = document.querySelector('#city-error');
-        const errorSpan = document.querySelector('#city-span');
-        let element = event.target;
-
-        removeAspValidation(errorSpan)
-
-        if (validateInputSwitch(element, errorMsg, "city"))
-            return true;
-    } catch { }
-}
-
-function validateEmailSwitch(event) {
-    try {
-        const errorMsg = document.querySelector('#email-error');
-        const errorSpan = document.querySelector('#email-span');
-        let element = event.target;
-
-        removeAspValidation(errorSpan)
-
-        if (validateInputSwitch(element, errorMsg, "email"))
-            return true;
-    } catch { }
-}
-function validatePasswordSwitch(event) {
-    try {
-        const errorMsg = document.querySelector('#password-error');
-        const errorSpan = document.querySelector('#password-span');
-        let element = event.target;
-
-        removeAspValidation(errorSpan)
-
-        if (validateInputSwitch(element, errorMsg, "password"))
-            return true;
-    } catch { }
-}
-
-function validateConfirmPasswordSwitch(event) {
-    try {
-        const errorMsg = document.querySelector('#confirm-password-error');
-        const errorSpan = document.querySelector('#confirm-password-span');
-        let element = event.target;
-
-        removeAspValidation(errorSpan)
-
-        if (validateInputSwitch(element, errorMsg, "confirm password"))
-            return true;
-    } catch { }
-}
-
-
-
-
 
 
 //Each value by its own error-validation
-function validateErrorFirstName() {
-    let firstNameOK = false;
-    const errorFirstName = document.querySelector('#first-name-error');
-    const inputFirstName = document.querySelector('#first-name-input');
+function validationErrorMsgPresent(label) {
+    const error = document.querySelector(`#${label}-error`);
+    const input = document.querySelector(`#${label}`);
 
-    if (inputFirstName.value !== "") {
-        if (errorFirstName.innerText === "") {
-            firstNameOK = true;
+    if (input.value !== "") {
+        if (error.innerText === "") {
+            return true;
         } else {
-            firstNameOK = false;
+            return false;
         }
     } else {
-        firstNameOK = false;
+        return false;
     }
+}
 
-    return firstNameOK;
+
+function validateErrorFirstName() {
+
+    return validationErrorMsgPresent("first-name");
 }
 
 function validateErrorLastName() {
-    let lastNameOK = false;
-    const errorLastName = document.querySelector('#last-name-error');
-    const inputLastName = document.querySelector('#last-name-input');
 
-    if (inputLastName.value !== "") {
-        if (errorLastName.innerText === "") {
-            lastNameOK = true;
-        } else {
-            lastNameOK = false;
-        }
-    } else {
-        lastNameOK = false;
-    }
-
-    return lastNameOK;
+    return validationErrorMsgPresent("last-name");
 }
 
 function validateErrorStreetName() {
-    let streetNameOK = false;
-    const errorStreetName = document.querySelector('#street-name-error');
-    const inputStreetName = document.querySelector('#street-name-input');
 
-    if (inputStreetName.value !== "") {
-        if (errorStreetName.innerText === "") {
-            streetNameOK = true;
-        } else {
-            streetNameOK = false;
-        }
-    } else {
-        streetNameOK = false;
-    }
-
-    return streetNameOK;
+    return validationErrorMsgPresent("street-name");
 }
 
 function validateErrorPostalCode() {
-    let postalCodeOK = false;
-    const errorPostalCode = document.querySelector('#postal-code-error');
-    const inputPostalCode = document.querySelector('#postal-code-input');
 
-    if (inputPostalCode.value !== "") {
-        if (errorPostalCode.innerText === "") {
-            postalCodeOK = true;
-        } else {
-            postalCodeOK = false;
-        }
-    } else {
-        postalCodeOK = false;
-    }
-
-    return postalCodeOK;
+    return validationErrorMsgPresent("postal-code");
 }
 
 function validateErrorCity() {
-    let cityOK = false;
-    const errorCity = document.querySelector('#city-error');
-    const inputCity = document.querySelector('#city-input');
 
-    if (inputCity.value !== "") {
-        if (errorCity.innerText === "") {
-            cityOK = true;
-        } else {
-            cityOK = false;
-        }
-    } else {
-        cityOK = false;
-    }
-
-    return cityOK;
+    return validationErrorMsgPresent("city");
 }
 
 function validateErrorEmail() {
-    let emailOK = false;
-    const errorEmail = document.querySelector('#email-error');
-    const inputEmail = document.querySelector('#email-input');
 
-    if (inputEmail.value !== "") {
-        if (errorEmail.innerText === "") {
-            emailOK = true;
-        } else {
-            emailOK = false;
-        }
-    } else {
-        emailOK = false;
-    }
-
-    return emailOK;
+    return validationErrorMsgPresent("email");
 }
 
 function validateErrorPassword() {
-    let passwordOK = false;
-    const errorPassword = document.querySelector('#password-error');
-    const inputPassword = document.querySelector('#password-input');
 
-    if (inputPassword.value !== "") {
-        if (errorPassword.innerText === "") {
-            passwordOK = true;
-        } else {
-            passwordOK = false;
-        }
-    } else {
-        passwordOK = false;
-    }
-
-    return passwordOK;
+    return validationErrorMsgPresent("password");
 }
 
 function validateErrorConfirmPassword() {
-    let confirmPasswordOK = false;
-    const errorConfirmPassword = document.querySelector('#confirm-password-error');
-    const inputConfirmPassword = document.querySelector('#confirm-password-input');
 
-    if (inputConfirmPassword.value !== "") {
-        if (errorConfirmPassword.innerText === "") {
-            confirmPasswordOK = true;
-        } else {
-            confirmPasswordOK = false;
-        }
-    } else {
-        confirmPasswordOK = false;
-    }
-
-    return confirmPasswordOK;
+    return validationErrorMsgPresent("confirm-password");
 }
 
-//Validation all by error-validation
+//Validation of all inputs
 function validateAll(event) {
     try {
         event.preventDefault();
 
-        const errorDiv = document.querySelector('#form-error');
+        const errorMsg = document.querySelector('#form-error');
 
         var firstNameOK = validateErrorFirstName();
         var lastNameOK = validateErrorLastName();
@@ -425,7 +218,7 @@ function validateAll(event) {
         if (firstNameOK && lastNameOK && streetNameOK && postalCodeOK && cityOK && emailOK && passwordOK && confirmPasswordOK)
             event.target.submit();
         else
-            errorDiv.innerText = "Please enter the form correctly.";
+            errorMsg.innerText = "Please enter the form correctly.";
 
     } catch { }
 }
